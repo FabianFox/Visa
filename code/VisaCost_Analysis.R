0fbd1fb244b0c39240c74c19559d45b0fc126e2d
FabianFox <fabianguelzau@web.de>
2020-12-01 17:03:20 +0100

    filter to Gleditsch/Ward states

--- code/VisaCost_Analysis.R
+++ code/VisaCost_Analysis.R
@@ -7,9 +7,28 @@
 # Load/install packages
 ### ------------------------------------------------------------------------###
 if (!require("xfun")) install.packages("xfun")
-pkg_attach2("tidyverse", "rio", "countrycode")
+pkg_attach2("tidyverse", "rio", "lubridate","countrycode", "states")
 
 # Load data
 ### ------------------------------------------------------------------------###
-cost.df <- import("./data/GMP Visa Cost/GMP_GlobalVisaCostDataset_v1.0.xlsx") %>%
+visa_cost.df <- import("./data/GMP Visa Cost/GMP_GlobalVisaCostDataset_v1.0.xlsx") %>%
   select(source_iso3, target_iso3, tourist_visa)
+
+# Filter to independent states
+### ------------------------------------------------------------------------###
+# Independent states as defined by Gleditsch & Ward (1999) 
+# data: http://ksgleditsch.com/data-4.html
+# Note: excluding microstates
+# Custom matches, i.e. 347 (Kosovo) = XKX
+custom.match <- c("260" = "DEU" ,"340" = "SRB", "347" = "RKI", "678" = "YEM")
+
+# Data
+states.df <- gwstates %>%
+  filter(year(end) == 9999 & microstate == FALSE) %>%
+  mutate(iso3c = countrycode(gwcode, "cown", "iso3c",     # original ISO3 is out-of-date
+                             custom_match = custom.match))
+
+# Subset
+visa_cost.df <- visa_cost.df %>%
+  filter(source_iso3 %in% states.df$iso3c &
+           target_iso3 %in% states.df$iso3c)
