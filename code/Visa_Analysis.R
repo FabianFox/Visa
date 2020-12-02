94e8f1e49b8220fab6ff37193d7066e675b7d442
FabianFox <fabianguelzau@web.de>
2020-12-02 15:11:12 +0100

    network formats

--- code/Visa_Analysis.R
+++ code/Visa_Analysis.R
@@ -9,8 +9,37 @@
 # Load/install packages
 ### ------------------------------------------------------------------------###
 if (!require("xfun")) install.packages("xfun")
-pkg_attach2("tidyverse", "rio", "countrycode", "patchwork", "statnet")
+pkg_attach2("tidyverse", "rio", "countrycode", "patchwork", "statnet", "ggraph", "tidygraph")
 
 # Load data
 ### ------------------------------------------------------------------------###
 visa.df <- import("./data/visa_2020.rds")
+
+# Network format(s)
+### ------------------------------------------------------------------------###
+# Transform into network format
+
+# Igraph
+# Create an igraph graph from data frame
+visa.graph <- graph_from_data_frame(visa.df[,c("destination_iso3", 
+                                               "nationality_iso3")],
+                                    directed = TRUE)
+
+# Set visa requirement as edge-attribute
+visa.graph <- set_edge_attr(visa.graph, "weight", 
+                            value = visa.df$visa_requirement_binary)
+
+# Transform into a matrix
+visa.mat <- get.adjacency(visa.graph, sparse = FALSE, 
+                               attr = "weight")
+
+# Tidygraph
+visa.tbl <- as_tbl_graph(visa.mat)
+
+# Network
+visa.net <- network(visa.mat)
+
+# Descriptive stats
+### ------------------------------------------------------------------------###
+# Triad census
+visa_triad.df <- triad.census(visa.mat)
